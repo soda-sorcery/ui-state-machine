862e241133e35a52bb8e8b05dba82681f99d93e4
dandalf <dandalf@lawvious.lan>
2020-03-16 20:44:04 -0600

    added ability to select an avatar

--- src/components/AvatarSelector/index.tsx
+++ src/components/AvatarSelector/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import {Name} from "../Name";
 import styled from '@emotion/styled/macro';
-import {AppBar, Container,Paper, Slide} from "@material-ui/core";
+import {AppBar, Container, Paper, Slide} from "@material-ui/core";
 import {IconNames} from "../Icon";
 import {Avatar} from "../Avatar";
 import { AvatarList } from "../AvatarList";
@@ -30,6 +30,11 @@ const AvatarSelector: React.FC = () => {
     setShouldShowList(false);
   };
 
+  const selectAvatarHandler = (event: any) => {
+    const avatar = event.target.id;
+    setSelectedAvatar(avatar);
+  };
+
   return (
     <>
      <Container onClick={handler}>
@@ -40,7 +45,7 @@ const AvatarSelector: React.FC = () => {
          </AppBar>
          <Slide direction={'down'} in={shouldShowList}>
            <Paper elevation={0} classes={{root: 'avatar-selection-list'}}>
-             <AvatarList />
+             <AvatarList avatarSelectorHandler={selectAvatarHandler}/>
            </Paper>
          </Slide>
        </FormContainer>
